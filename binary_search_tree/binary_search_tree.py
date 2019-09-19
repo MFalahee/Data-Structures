b8677444146a99c7530fbab94b3d5eed0dee177c
mjfalahee <mjfalahee@gmail.com>
2019-09-19 14:21:35 -0700

    MVP done for Day on Binary Search Trees

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,4 @@
 import sys
-sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -100,18 +99,79 @@ class BinarySearchTree:
 
   # Print all the values in order from low to high
   # Hint:  Use a recursive, depth first traversal
+
+    # depth first --> go down (pick a direction) until we run into a dead end
+    # once you hit a dead end, circle back one and go the other way
+    # continue until we hit all of the nodes
+    # depth first stack, sort the entries into the stack I'm guessing? Conditional of some sort.
+
   def in_order_dft(self, node):
-      pass
+      #check left side, recursively until we hit a dead end to the left (lowest number)
+      if node.left is not None:
+        node.left.in_order_dft(node.left)
+
+      #Print value!
+      print(node.value)
+
+      #move right when the value is printed
+      if node.right is not None:
+        node.right.in_order_dft(node.right)
+
 
   # Print the value of every node, starting with the given node,
   # in an iterative breadth first traversal
   def bft_print(self, node):
-      pass
+      #print first node
+      print(node.value)
+
+      #initialize queue
+      store = Queue()
+
+      #add first values to queue
+      if node.left is not None:
+        store.enqueue(node.left)
+      if node.right is not None:
+        store.enqueue(node.right)
+
+      while store.len() > 0:
+        current_node = store.dequeue()
+        print(current_node.value)
+
+        if current_node.left is not None:
+          store.enqueue(current_node.left)
+        if current_node.right is not None:
+          store.enqueue(current_node.right)
 
   # Print the value of every node, starting with the given node,
   # in an iterative depth first traversal
+  # when stack is empty, we have traversed the entire tree, we are done
   def dft_print(self, node):
-      pass
+      #print first node
+      print(node.value)
+
+      #initialize stack
+      store = Stack()
+
+      #push in first children so Stack isn't empty when we start iterating the loop
+      if node.left is not None:
+        store.push(node.left)
+      if node.right is not None:
+        store.push(node.right)
+
+      while store.len() > 0:
+        current_node = store.pop()
+        print(current_node.value)
+
+        if current_node.left is not None:
+          store.push(current_node.left)
+        if current_node.right is not None:
+          store.push(current_node.right)
+
+
+
+
+
+
 
   # STRETCH Goals -------------------------
   # Note: Research may be required
