02b4b27fddd2b8f5934736c688d2392869002ec2
mjfalahee <mjfalahee@gmail.com>
2019-09-19 13:10:31 -0700

    Day 2 problems and tests added

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
-# import sys
-# sys.path.append('../queue_and_stack')
-# from dll_queue import Queue
-# from dll_stack import Stack
+import sys
+sys.path.append('../queue_and_stack')
+from dll_queue import Queue
+from dll_stack import Stack
 
 '''
 Lecture Notes
@@ -87,7 +87,7 @@ class BinarySearchTree:
   # the passed-in callback function on each tree node value. There is a myriad of ways to
   # perform tree traversal; in this case any of them should work. 
   def for_each(self, cb):
-    
+
     cb(self.value)
 
     if self.right != None:
@@ -95,6 +95,31 @@ class BinarySearchTree:
 
     if self.left != None:
       self.left.for_each(cb)
-      
 
+  # DAY 2 Project -----------------------
 
+  # Print all the values in order from low to high
+  # Hint:  Use a recursive, depth first traversal
+  def in_order_dft(self, node):
+      pass
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative breadth first traversal
+  def bft_print(self, node):
+      pass
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative depth first traversal
+  def dft_print(self, node):
+      pass
+
+  # STRETCH Goals -------------------------
+  # Note: Research may be required
+
+  # Print In-order recursive DFT
+  def pre_order_dft(self, node):
+      pass
+
+  # Print Post-order recursive DFT
+  def post_order_dft(self, node):
+      pass
